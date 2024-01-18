a45e31f3d1ab86d39b3800bde269e5db453bc65d
brobit <brobit@pop-os.localdomain>
2024-01-18 12:27:54 +0100

    forgot one file

--- Sources/Instructions/swap_a.c
+++ Sources/Instructions/swap_a.c
@@ -6,7 +6,7 @@
 /*   By: almarico <almarico@student.42lehavre.fr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 17:15:22 by almarico          #+#    #+#             */
-/*   Updated: 2024/01/18 12:13:19 by almarico         ###   ########.fr       */
+/*   Updated: 2024/01/18 12:27:08 by almarico         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,5 @@ void	ft_swap_a(t_get *get)
 	tmp = first->next->next;
 	second->next = first;
 	first->next = tmp;
+	get->stack_a = second;
 }
